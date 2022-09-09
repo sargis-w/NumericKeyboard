70198f4b230efa65ca55f5a73debf5b2be49ac73
Employee7 <employee7@Employee7s-iMac.local>
2022-09-09 11:42:56 +0400

    Fixed errors

---
+++ Example/build/Pods.build/Debug-iphonesimulator/Pods-NumericKeyboard_Tests.build/DerivedSources/Pods_NumericKeyboard_Tests_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_NumericKeyboard_TestsVersionString[];
+ extern const double Pods_NumericKeyboard_TestsVersionNumber;
+
+ const unsigned char Pods_NumericKeyboard_TestsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_NumericKeyboard_Tests  PROJECT:Pods-1" "\n";
+ const double Pods_NumericKeyboard_TestsVersionNumber __attribute__ ((used)) = (double)1.;
